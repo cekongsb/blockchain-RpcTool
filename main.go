72c1241349932568a9bcafa062c685b72b470546
cekongsb <806138244@qq.com>
2020-04-21 23:42:43 +0800

    commit 4-21 night

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 )
 
 const (
-	SERVER_HOST       = "192.168.0.143"
-	SERVER_PORT       = 6062
+	SERVER_HOST       = "192.168.8.107"
+	SERVER_PORT       = 9380
 	USER              = "chain"
 	PASSWD            = "999000"
 	USESSL            = false
@@ -22,11 +23,27 @@ func main() {
 	// reqJson := "{\"method\":\"getnewaddress\",\"params\":[\"labelName002\"],\"id\":1}"
 	// reqJson := "{\"method\":\"listunspent\",\"params\":[],\"id\":1}"
 
-	reqJson := "{\"method\":\"listunspent\",\"params\":[1, 9999, [], true, {\"minimumSumAmount\":100}],\"id\":1}"
+	// reqJson := "{\"method\":\"listunspent\",\"params\":[1, 9999999, [], true, {\"minimumSumAmount\":100}],\"id\":1}"
+	reqJson := `{"method":"listunspent","params":[1, 9999999, [], true, {"minimumSumAmount":2}],"id":1}`
 
 	returnJson, err2 := rpcClient.send(reqJson)
 	if err2 != nil {
 		log.Fatalln(err2)
 	}
 	log.Println("returnJson:", returnJson)
+
+	var rpcresponse rpcResponse
+	err3 := json.Unmarshal([]byte(returnJson), &rpcresponse)
+	if err3 != nil {
+		log.Fatalln(err3)
+	}
+	log.Println(string(rpcresponse.Result))
+
+	var resultInfo interface{}
+	err4 := json.Unmarshal(rpcresponse.Result, &resultInfo)
+	if err4 != nil {
+		log.Fatalln(err4)
+	}
+	log.Println(resultInfo)
+	// log.Println(resultInfo)
 }
