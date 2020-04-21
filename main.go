57efea4fd035669e4f737f9622d7307fa16e79bb
cekonsb <806138244@qq.com>
2020-04-21 17:33:48 +0800

    commit 4-21

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import (
 )
 
 const (
-	SERVER_HOST       = "192.168.0.125"
-	SERVER_PORT       = 9380
+	SERVER_HOST       = "192.168.0.143"
+	SERVER_PORT       = 6062
 	USER              = "chain"
 	PASSWD            = "999000"
 	USESSL            = false
@@ -19,7 +19,11 @@ func main() {
 		log.Fatalln(err)
 	}
 	//生成一个新地址
-	reqJson := "{\"method\":\"getnewaddress\",\"params\":[\"labelName002\"],\"id\":1}"
+	// reqJson := "{\"method\":\"getnewaddress\",\"params\":[\"labelName002\"],\"id\":1}"
+	// reqJson := "{\"method\":\"listunspent\",\"params\":[],\"id\":1}"
+
+	reqJson := "{\"method\":\"listunspent\",\"params\":[1, 9999, [], true, {\"minimumSumAmount\":100}],\"id\":1}"
+
 	returnJson, err2 := rpcClient.send(reqJson)
 	if err2 != nil {
 		log.Fatalln(err2)
